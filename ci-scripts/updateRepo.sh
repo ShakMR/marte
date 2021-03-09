991ac49dbca02b1bd22d0f61ed2040b258f95b0b
ShakMR <borja.arias.upc@gmail.com>
2021-03-09 19:52:01 +0100

    Update workspace path in circle ci

--- ci-scripts/updateRepo.sh
+++ ci-scripts/updateRepo.sh
@@ -4,3 +4,4 @@ LC_PROJECT_NAME=marte
 ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com "mkdir -p Projects/${LC_PROJECT_NAME}" && \
 rsync -rcplz --verbose ${PWD}/../* suilabs@suilabs.com:./Projects/${LC_PROJECT_NAME}/. && \
 ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com "cd Projects/${LC_PROJECT_NAME}; docker-compose up -d --build"
+
