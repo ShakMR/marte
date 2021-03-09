5d1e8021fed993b3bcc00a475665094e4d29337a
ShakMR <borja.arias.upc@gmail.com>
2021-03-09 19:21:17 +0100

    Try to make RSYNC update the files

--- ci-scripts/updateRepo.sh
+++ ci-scripts/updateRepo.sh
@@ -2,5 +2,5 @@
 
 LC_PROJECT_NAME=marte
 ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com "mkdir -p Projects/${LC_PROJECT_NAME}" && \
-rsync -rpulz --verbose ${PWD}/../* suilabs@suilabs.com:./Projects/${LC_PROJECT_NAME}/. && \
+rsync -rplz --verbose ${PWD}/../* suilabs@suilabs.com:./Projects/${LC_PROJECT_NAME}/. && \
 ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com "cd Projects/${LC_PROJECT_NAME}; docker-compose up -d --build"
